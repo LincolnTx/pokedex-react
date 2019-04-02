04b49e7e2cc980c2f98a98443d1c4a1e6395b8c3
LincolnTx <lincolnsf98@hotmail.com>
2019-04-02 12:03:31 -0300

    Criando componente de navBar

---
+++ src/components/layout/NavBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import styled from 'styled-components';
+
+
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top" >
+            <a>Pokedex</a>
+        </nav>
+      </div>
+    )
+  }
+}
